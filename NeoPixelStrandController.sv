eac0e6cd3294003731dbb6a82d73195fb668ec0e
Vivian Cheng <vccheng@andrew.cmu.edu>
2020-12-02 23:52:26 -0500

    Commenting + localparams

--- NeoPixelStrandController.sv
+++ NeoPixelStrandController.sv
@@ -42,7 +42,17 @@ module NeoPixelStrandController
   counter #(7) cycle (.en(cycle_en), .clear(cycle_clear), .q(cycle_count),
                               .d(7'd0), .clock(clock), .reset(reset));
 
+ // Number of cycles for sending 1-Bit
+ localparam BIT_1_HIGH = 7'd35;
+ localparam BIT_1_LOW  = 7'd30; 
 
+ // Number of cycles for sending 0-Bit
+ localparam BIT_0_HIGH  = 7'd18;
+ localparam BIT_0_LOW  = 7'd40; 
+
+ // Display packet length 
+ localparam NUM_BITS = 7'd120;
+ 
 
   // Wait 50 microseconds between each display packet
   logic [11:0] wait50_count;
@@ -53,6 +63,8 @@ module NeoPixelStrandController
 
 
 
+  // done_high when done waiting for cycles where asserted high 
+  // done_low when done waiting for cycles where asserted low 
   logic done_high, done_low; 
 
   // States
@@ -68,11 +80,15 @@ module NeoPixelStrandController
     wait50_en = 0; wait50_clear = 1;
     cycle_en = 0; cycle_clear = 1;
     case (currstate)
+
+      // Upon reset, ready to load/send. 
+      // No bits have been sent yet (send_clear=1), RGB all set to 0
       RESET: begin
-        // Upon reset, ready to load/send 
+
         send_en = 0; send_clear = 1;
         ready_to_load = 1; ready_to_send = 1;
         G = 40'd0; R = 40'd0; B = 40'd0;
+
         // Load a specified color value into R, G, or B for one LED 
         if (load_color) begin 
            nextstate = LOAD;
@@ -81,16 +97,22 @@ module NeoPixelStrandController
               2'b01: B[pixel_index] = color_level; // Blue
               2'b10: G[pixel_index] = color_level; // Green 
            endcase
-        // Send 
-        end else if (send_it) nextstate = SEND;
+        end
+        // Send  
+        else if (send_it) nextstate = SEND;
         // Stay in IDLE
         else nextstate = RESET;
       end
+
+
+      // Available to load 
       LOAD: begin
         send_en = 0; send_clear = 1;
         ready_to_load = 1; ready_to_send = 1;
-        // Load a specified color value into R, G, or B for one LED 
+
+        // Send 
         if (send_it) nextstate = SEND;
+        // Load 
         else begin
           nextstate = LOAD;
           if (load_color) begin 
@@ -102,22 +124,25 @@ module NeoPixelStrandController
           end
         end
       end
-      // SEND Serial bit stream 
+
+      // Send display packet of 120 bits 
       SEND: begin 
+        // Every time send a new bit, reset done_high and done_low to 0
         done_high = 0; done_low = 0;
 
         // Iterated through all 120 pixels in display packet 
-        if (send_count == 7'd120) begin 
-            neo_data = 1'bx;
-            nextstate = WAIT;
-            send_en = 0; send_clear = 1;
-            ready_to_load = 1; ready_to_send = 0;
-            cycle_en = 0; cycle_clear = 1;
+        if (send_count == NUM_BITS) begin 
+            neo_data = 1'bx;  // Don't send any more neo_data
+            nextstate = WAIT; // wait 50 microseconds 
+            send_en = 0; send_clear = 1;  // clear 
+            ready_to_load = 1; ready_to_send = 0; // can load while waiting 
+            cycle_en = 0; cycle_clear = 1; // clear cycle counts 
 
         end else begin
 
             nextstate = SEND; 
             send_en = 1; send_clear = 0;
+            // Deassert while sending 
             ready_to_load = 0; ready_to_send = 0; 
             
             // Send 1-bit
@@ -126,6 +151,7 @@ module NeoPixelStrandController
               neo_data = 1; 
               cycle_en = 1; cycle_clear = 0;
             end 
+
             // Send 0-bit
             else if (display_packet[send_count] == 0) begin 
               nextstate = SEND0;
@@ -139,30 +165,36 @@ module NeoPixelStrandController
     /******************************************************************/
     /*       SEND A ONE-BIT: 35 cycles high, 30 cycles low            */
     /******************************************************************/
-
+        
+      // One-bit: 35 cycles high, 30 cycles low 
       SEND1: begin 
         send_en = 0; send_clear = 0; 
-        // One-bit: 35 cycles high, 30 cycles low 
+
+        // Sending high bits 
         if (!done_high & !done_low) begin
           nextstate = SEND1;  
-          if (cycle_count == 35) begin 
-            neo_data = 0; 
+          // Finished 35 high 
+          if (cycle_count == BIT_1_HIGH) begin 
+            neo_data = 0;  // send first low bit, keep counting 
             done_high = 1; cycle_en = 1; cycle_clear = 0; 
+          // Not finished 35 high 
           end else begin 
-            neo_data = 1;
+            neo_data = 1; // send high bits, keep counting 
             done_high = 0; cycle_en = 1; cycle_clear = 0;  
           end 
         end 
 
-        // Finished 35 cycles of ones
+        // Finished 35 high
         else if (done_high & !done_low) begin 
-          if (cycle_count == 64) begin  // 35 + 30 - 1
+          // Finished 30 low
+          if (cycle_count == (BIT_1_HIGH + BIT_1_LOW - 1)) begin  // 35 + 30 - 1
             nextstate = SEND;
-            neo_data = 0;
+            neo_data = 0; // send last low bit, stop counting 
             done_low = 1; cycle_en = 0; cycle_clear = 1;  
+          // Not finished 30 low
           end else begin 
             nextstate = SEND1; 
-            neo_data = 0; 
+            neo_data = 0; // send low bits, keep counting 
             done_low = 0; cycle_en = 1; cycle_clear = 0;
           end 
         end 
@@ -171,30 +203,36 @@ module NeoPixelStrandController
     /******************************************************************/
     /*       SEND A ZERO-BIT: 18 cycles high, 40 cycles low            */
     /******************************************************************/
-
+      
+      // Zero-bit: 18 cycles high, 40 cycles low 
       SEND0: begin 
         send_en = 0; send_clear = 0;
-        // Zero-bit: 18 cycles high, 40 cycles low 
+
+        // Sending high bits 
         if (!done_high & !done_low) begin
           nextstate = SEND0;  
-          if (cycle_count == 18) begin 
-            neo_data = 0; 
-            done_high = 1; cycle_en = 1; cycle_clear = 0; 
+          // Finished 18 high 
+          if (cycle_count == BIT_0_HIGH) begin 
+            neo_data = 0;  // send first low bit, keep counting 
+            done_high = 1; cycle_en = 1; cycle_clear = 0;
+          // Not finished 18 high 
           end else begin 
-            neo_data = 1;
+            neo_data = 1; // send high bits, keep counting 
             done_high = 0; cycle_en = 1; cycle_clear = 0;  
           end 
         end 
 
-        // Finished 35 cycles of ones
+        // Sending low bits 
         else if (done_high & !done_low) begin 
-          if (cycle_count == 57) begin  // 18 + 40 - 1
+          // Finished 40 low
+          if (cycle_count == (BIT_0_HIGH + BIT_0_LOW - 1)) begin  // 18 + 40 - 1
             nextstate = SEND;
-            neo_data = 0;
-            done_low = 1; cycle_en = 0; cycle_clear = 1;  
+            neo_data = 0; // send last low bit, stop counting 
+            done_low = 1; cycle_en = 0; cycle_clear = 1;
+          // Not finished 40 low
           end else begin 
             nextstate = SEND0; 
-            neo_data = 0; 
+            neo_data = 0; // send low bits, keep counting  
             done_low = 0; cycle_en = 1; cycle_clear = 0;
           end 
         end 
