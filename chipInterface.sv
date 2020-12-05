9242ca65189c448692d902328949ca75eef338ce
Vivian Cheng <vccheng@andrew.cmu.edu>
2020-12-05 02:16:18 -0500

    chipInterface reset synced Key0

--- chipInterface.sv
+++ chipInterface.sv
@@ -2,10 +2,9 @@
 
 module chipInterface
   (input  logic CLOCK_50,
+   input logic [3:0] KEY,
    output logic [31:0] GPIO_0); 
  
-   logic reset;
-
    logic [2:0] pixel_index;          
    logic [1:0] color_index;
    logic [7:0] color_level;
@@ -13,9 +12,19 @@ module chipInterface
    logic load_color, send_it;        
    logic neo_data, ready_to_load, ready_to_send;
 
+   logic clock, reset;
+
+   logic syncedKEY2_temp, syncedKEY2;   // synchronized output for KEY2
+  
+   // Flip flop synchronizers for KEY[2] to reduce metastability
+   register #(1) sync2_0 (.q(syncedKEY2_temp), .d(KEY[2]), .en(1'b1), .clock(CLOCK_50), .clear(1'b0), .reset(1'b0));
+   register #(1) sync2_1 (.q(syncedKEY2), .d(syncedKEY2_temp), .en(1'b1), .clock(CLOCK_50), .clear(1'b0), .reset(1'b0));
+
+   assign clock = CLOCK_50;    // 50 MHz clock
+   assign reset = ~syncedKEY2; // reset when KEY2 pressed 
    assign GPIO_0[1] = neo_data;
    
-   NeoPixelStrandController np (.clock(CLOCK_50), .*);
-   Task2 t2 (.clock(CLOCK_50), .*);
+   NeoPixelStrandController np (.*);
+   Task2 t2 (.*);
    
 endmodule: chipInterface 
