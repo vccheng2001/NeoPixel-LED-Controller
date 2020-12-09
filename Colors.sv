307f4c9359b63dc2be539a55aab72ca91eff4503
vccheng <vccheng@DESKTOP-VI0PAKM.localdomain>
2020-12-08 18:59:40 -0800

    Updated 12/9, Neon/Rainbow/Christmas.

--- Colors.sv
+++ Colors.sv
@@ -36,7 +36,7 @@ module Colors
            // Special Neon mode: Rainbow mode!!!
                 color_hues = {8'h20, 8'h10, 8'h05, 8'h00};
                 // SW[1] determines rate of blinking
-                max_num_loads = (syncedSW[1]) ? 7'd63 : 7'd31;
+                max_num_loads = 7'd63;
            end
            color_array = NEON_COLOR_ARRAY;
            pixel_array = NEON_PIXEL_ARRAY;
@@ -52,7 +52,7 @@ module Colors
    end
 
  // Neon color array: encodes color index
- assign NEON_COLOR_ARRAY = { C0, C1, C0, C1, C1, C3, C2, C0, C3, C2, C3, C0, C1, C3, C1, 
+ assign NEON_COLOR_ARRAY = {    C0, C1, C0, C1, C1, C3, C2, C0, C3, C2, C3, C0, C1, C3, C1, 
                                 C1, C2, C3, C2, C0, C2, C2, C0, C3, C1, C0, C0, C0, C3, C2, 
                                 C0, C3, C1, C2, C3, C1, C1, C2, C2, C0, C2, C1, C3, C2, C0,
                                 C0, C1, C0, C1, C1, C0, C0, C1, C3, C3, C3, C2, C2, C0, C1,
